f863dc053ce07b712d63b80351b082dd6bc231c1
Attila Bartha <atikenny@gmail.com>
2017-02-22 16:47:27 +0000

    add polyfills, refactor modules

--- zoom-scroller.js
+++ zoom-scroller.js
@@ -1,4 +1,13 @@
-import bodyInliner from './lib/body-inliner';
+import {
+    drawSvgToCanvasAsync,
+    drawWhereYouAtWindow
+} from './lib/canvas-helpers';
+
+import {
+    addStyleToElement,
+    appendElement,
+    fadeInElement
+} from './lib/dom-helpers';
 
 (() => {
     'use strict';
@@ -9,105 +18,45 @@ import bodyInliner from './lib/body-inliner';
     };
 
     let ticking = false;
-    const hasRequestAnimationFrame = !!window.requestAnimationFrame;
 
-    const appendCanvas = (blurScroller) => {
-        const canvas = document.createElement('canvas');
-        
-        canvas.style.position = 'fixed';
-        canvas.style.top = 0;
-        canvas.style.left = 0;
-        canvas.style.transformOrigin = 'top right';
-        canvas.style.cursor = 'pointer';
-
-        if (blurScroller) {
-            canvas.style.filter = 'blur(4px)';
-        }
-
-        canvas.style.transition = 'all .2s linear';
-        
-        fadeOutElement(canvas);
-        
-        document.body.appendChild(canvas);
-
-        return canvas;
-    };
-
-    const getSvgUrl = (domUrl, inlinedHTML) => {
-        const getSVG = (inlinedHTML) => {
-            const data = '<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"><foreignObject width="100%" height="100%"><div xmlns="http://www.w3.org/1999/xhtml">{{html}}</div></foreignObject></svg>';
-            
-            return data.replace('{{html}}', inlinedHTML);  
+    const appendCanvas = (parentElement, blurScroller) => {
+        const canvasStyles = {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            transformOrigin: 'top right',
+            cursor: 'pointer',
+            transition: 'all .2s linear',
+            visibility: 'hidden',
+            opacity: 0,
+            filter: blurScroller ? 'blur(4px)' : ''
         };
 
-        const svg = new Blob([getSVG(inlinedHTML)], { type: 'image/svg+xml' });
-        
-        return domUrl.createObjectURL(svg);
+        return appendElement(parentElement, 'canvas', canvasStyles);
     };
 
-    const createImageOnCanvas = (canvas, domUrl, svgUrl) => {
-        const context = canvas.getContext('2d');
-        const img = new Image();
-
-        img.onload = (event) => {
-            clearCanvas(canvas);
-            context.drawImage(img, 0, 0);
-            domUrl.revokeObjectURL(svgUrl);
-
-            drawWhereYouAtWindow(context);
-
-            ticking = false;
-        };
-
-        img.src = svgUrl;
-    };
-
-    const clearCanvas = (canvas) => {
-        const context = canvas.getContext('2d');
-        context.clearRect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
-    };
-
-    const scaleCanvas = (canvas, scrollerWidth, isScrollEvent) => {
+    const scaleCanvas = (parentElement, canvas, scrollerWidth, isScrollEvent) => {
         const getScrollerScale = (targetWidth) => {
-            const scale = 1 / (document.body.scrollWidth / targetWidth);
+            const scale = 1 / (parentElement.scrollWidth / targetWidth);
 
             return scale;
         };
 
         const scrollerScale = getScrollerScale(scrollerWidth);
 
-        canvas.setAttribute('width', document.body.scrollWidth);
-        canvas.setAttribute('height', document.body.scrollHeight);
+        canvas.width = parentElement.scrollWidth;
+        canvas.height = parentElement.scrollHeight;
 
-        canvas.style.transform = `scale(${scrollerScale}, ${scrollerScale})`;
+        addStyleToElement(canvas, { transform: `scale(${scrollerScale}, ${scrollerScale})` });
     };
 
-    const drawWhereYouAtWindow = (context) => {
-        const rectStartY = document.body.scrollTop;
-        const rectWidth = document.body.clientWidth;
-        const rectHeight = document.body.clientHeight;
-
-        context.globalCompositeOperation = 'darken';
-        context.fillStyle = 'grey';
-        context.fillRect(0, rectStartY, rectWidth, rectHeight);
-    };
-
-    const fadeOutElement = (element) => {
-        element.style.visibility = 'hidden';
-        element.style.opacity = 0;
-    };
-
-    const fadeInElement = (element) => {
-        element.style.visibility = 'visible';
-        element.style.opacity = 1;
-    };
-
-    const attachScrollAndResizeEvents = (element, canvas, domUrl, scrollerWidth, inlinedHTML) => {
+    const attachScrollAndResizeEvents = (parentElement, window, canvas, scrollerWidth) => {
         const scaleCallback = (isScrollEvent) => {
-            createImageOnCanvas(canvas, domUrl, getSvgUrl(domUrl, inlinedHTML));
+            drawSvgToCanvasAsync(canvas, window, parentElement)
+                .then(onEndDrawingSvg);
 
             if (!isScrollEvent) {
-                scaleCanvas(canvas, scrollerWidth);
+                scaleCanvas(parentElement, canvas, scrollerWidth);
             }
         };
 
@@ -121,13 +70,13 @@ import bodyInliner from './lib/body-inliner';
             ticking = true;
         };
 
-        element.addEventListener('resize', eventHandler);
-        element.addEventListener('scroll', eventHandler);
+        window.addEventListener('resize', eventHandler);
+        window.addEventListener('scroll', eventHandler);
     };
 
-    const attachCanvasClickEvent = (body, canvas) => {
+    const attachCanvasClickEvent = (parentElement, canvas) => {
         const eventHandler = (event) => {
-            body.scrollTop = event.offsetY - (body.clientHeight / 2);
+            parentElement.scrollTop = event.offsetY - (parentElement.clientHeight / 2);
         };
 
         canvas.addEventListener('click', eventHandler);
@@ -139,16 +88,21 @@ import bodyInliner from './lib/body-inliner';
         });
     };
 
-    const domUrl = window.URL || window.webkitURL || window;
+    const parentElement = document.body;
 
-    const inlinedHTML = bodyInliner.getHTML();
-    const canvas = appendCanvas(config.blurScroller);
-    const svgUrl = getSvgUrl(domUrl, inlinedHTML);
+    const canvas = appendCanvas(parentElement, config.blurScroller);
 
-    createImageOnCanvas(canvas, domUrl, svgUrl);
-    scaleCanvas(canvas, config.scrollerWidth);
+    const onEndDrawingSvg = () => {
+        drawWhereYouAtWindow(parentElement, canvas);
+        ticking = false;
+    };
+
+    drawSvgToCanvasAsync(canvas, window, parentElement)
+        .then(onEndDrawingSvg);
+
+    scaleCanvas(parentElement, canvas, config.scrollerWidth);
     fadeInElement(canvas);
 
-    attachScrollAndResizeEvents(window, canvas, domUrl, config.scrollerWidth, inlinedHTML);
-    attachCanvasClickEvent(document.body, canvas);
+    attachScrollAndResizeEvents(parentElement, window, canvas, config.scrollerWidth);
+    attachCanvasClickEvent(parentElement, canvas);
 })();
